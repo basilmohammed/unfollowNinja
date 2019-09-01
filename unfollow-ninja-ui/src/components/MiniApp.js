e32f4960fd435bde89c2601569b8a66a33edb754
plhery <paullouis.hery@gmail.com>
2019-09-01 20:45:28 +0200

    add a temporary warning to the website

--- unfollow-ninja-ui/src/components/MiniApp.js
+++ unfollow-ninja-ui/src/components/MiniApp.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {Box, Button} from "grommet/es6";
-import {Twitter, ChatOption, UserExpert} from "grommet-icons";
+import {Box, Button, Paragraph} from "grommet/es6";
+import {Alert, ChatOption, Twitter, UserExpert} from "grommet-icons";
+import {Link} from "./index";
 
 export default (props) => (
     <Box gap='small' margin={{horizontal: 'small', vertical: 'medium'}} {...props}>
+    <Paragraph textAlign='center'><Alert/><br/>Suite à un problème avec Twitter, l'application n'est plus disponible, mais sera de retour bientôt. Suivez <Link href='https://twitter.com/unfollowNinja'>@unfollowNinja</Link> pour plus d'infos</Paragraph>
       <Button
           icon={<Twitter color='white'/>}
           label='Connectez-vous à votre compte'
           primary={true}
           style={{color: 'white'}}
+          disabled={true}
       />
       <Button
           icon={<ChatOption/>}
