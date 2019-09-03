b307742142e55ce2e6f69b9e991c913de17d4994
plhery <paullouis.hery@gmail.com>
2019-09-03 15:50:02 +0200

    change warning message

--- unfollow-ninja-ui/src/components/MiniApp.js
+++ unfollow-ninja-ui/src/components/MiniApp.js
@@ -5,7 +5,7 @@ import {Link} from "./index";
 
 export default (props) => (
     <Box gap='small' margin={{horizontal: 'small', vertical: 'medium'}} {...props}>
-    <Paragraph textAlign='center'><Alert/><br/>Suite à un problème avec Twitter, l'application n'est plus disponible, mais sera de retour bientôt. Suivez <Link href='https://twitter.com/unfollowNinja'>@unfollowNinja</Link> pour plus d'infos</Paragraph>
+    <Paragraph textAlign='center'><Alert/><br/>Le service est surchargé, mais sera probablement de retour ce soir. Suivez <Link href='https://twitter.com/unfollowNinja'>@unfollowNinja</Link> pour plus d'infos</Paragraph>
       <Button
           icon={<Twitter color='white'/>}
           label='Connectez-vous à votre compte'
